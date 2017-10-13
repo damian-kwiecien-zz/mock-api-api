c7ba752fc5682aa85d7a6c39584b3dfc7e94a881
Damian <damian.kwiecien@gorrion.pl>
2017-10-13 12:24:34 +0200

    setup typeorm

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,22 @@
 import * as express from 'express'
 import * as cors from 'cors'
 import * as bodyParser from 'body-parser'
+import { createConnection } from "typeorm"
 
 const app = express()
 const port = 4400
+const connection = createConnection()
+
+process.on('unhandledRejection', err => {
+    console.log('unhandledRejection', err)
+})
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
 
 app.get('/', (req, res, next) => {
-    res.json('Hello world')
+    connection.then(c => res.json(c.name))
 })
 
 app.listen(port, () => {
