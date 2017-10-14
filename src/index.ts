7db1dfd872999a611e02c2aa8a44dfc173517f28
Damian <damian.kwiecien@gorrion.pl>
2017-10-14 17:32:22 +0200

    configure typeorm as async instance

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,36 @@
 import * as express from 'express'
 import * as cors from 'cors'
 import * as bodyParser from 'body-parser'
-import { createConnection } from "typeorm"
+import { connection } from './instances'
+import { userRouter } from './routers/user.router'
+import { tokenGuard } from './middlewares'
 
-const app = express()
-const port = 4400
-const connection = createConnection()
+const init = async () => {
+    const userRouterSync = await userRouter
 
-process.on('unhandledRejection', err => {
-    console.log('unhandledRejection', err)
-})
+    const app = express()
+    const port = 4400
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(cors())
 
-app.get('/', (req, res, next) => {
-    connection.then(c => res.json(c.name))
-})
+    process.on('unhandledRejection', err => {
+       console.log('unhandledRejection', err)
+    })
 
-app.listen(port, () => {
-    console.log(`App is listening on port ${port}`)
-})
+    app.use(bodyParser.json())
+    app.use(bodyParser.urlencoded({ extended: true }))
+    app.use(cors())
+
+    app.use('/', userRouterSync)
+    app.use(tokenGuard())
+
+    app.get('/', (req, res, next) => {
+        res.json('awawd')
+    })
+
+    app.listen(port, () => {
+        console.log(`App is listening on port ${port}`)
+    })
+
+}
+
+init()
