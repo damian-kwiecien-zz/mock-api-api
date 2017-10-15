eb3e7d4b10eab096d52fcb9ee6cdcfe432aa65ac
Damian <damian.kwiecien@gorrion.pl>
2017-10-15 17:40:44 +0200

    refactor user router

--- src/routers/user.router.ts
+++ src/routers/user.router.ts
@@ -1,37 +1,33 @@
 import { Router } from 'express'
 import { matchedData } from 'express-validator/filter'
-import { validationResult } from 'express-validator/check'
-import { userRules } from '../rules/user.rules'
-import { UserService } from '../services/user.service'
-import { UserRegisterModel, UserLoginModel } from '../entities/user'
+import { userRules } from '../rules'
+import { UserService } from '../services'
+import { UserRegisterModel, UserLoginModel } from '../entities'
+import { bodyValidator, tokenGuard } from '../middlewares'
 
 const createUserRouter = async () => {
     const userRulesSync = await userRules
     const userRouter = Router()
     const userService = new UserService()
 
-    userRouter.post('/register', userRulesSync['forRegister'], (req, res) => {
-        const errors = validationResult(req)
-
-        if (!errors.isEmpty())
-            return res.status(422).json(errors.array())
-
+    userRouter.post('/register', userRulesSync['forRegister'], bodyValidator(), async (req, res) => {
         const payload = matchedData(req) as UserRegisterModel
-        const user = userService.register(payload)
+        const user = await userService.register(payload)
 
-        return user.then(u => res.json(u))
+        return res.json(user)
     })
 
-    userRouter.post('/login', userRulesSync['forLogin'], (req, res) => {
-        const errors = validationResult(req)
-
-        if (!errors.isEmpty())
-            return res.status(422).json(errors.array())
-
+    userRouter.post('/login', userRulesSync['forLogin'], bodyValidator(), async (req, res) => {
         const payload = matchedData(req) as UserLoginModel
-        const token = userService.login(payload)
+        const token = await userService.login(payload)
 
-        return token.then(t => res.json(t))
+        return res.json(token)
+    })
+
+    userRouter.use(tokenGuard())
+
+    userRouter.post('/check', userRulesSync['forCheck'], bodyValidator(), async (req, res) => {
+        return res.json({ message: 'Ok' })
     })
 
     return userRouter
